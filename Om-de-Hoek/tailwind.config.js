1f8eb5d1f90bdb8a9b8fb482bc54d4545b89e281
xanderdrvr <xander.deroover@student.ucll.be>
2025-11-12 15:50:19 +0100

    Upgrade to SDK 54 & install nativewind

---
+++ Om-de-Hoek/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: '#2548BC',
+        red: '#CB0000',
+        white: '#FFFFFF',
+        black: '#100D08',
+        gray: '#828282', 
+      }
+    },
+  },
+  plugins: [],
+}
+
