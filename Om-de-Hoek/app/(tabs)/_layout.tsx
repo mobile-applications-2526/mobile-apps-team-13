c5a240bb205c86fe0e60ca369bf347b3e038c515
Robbe Caerts <113980548+RobbeCaerts@users.noreply.github.com>
2025-11-12 21:41:07 +0100

    feat: Updated PressableButton.tsx to change color on press

--- Om-de-Hoek/app/(tabs)/_layout.tsx
+++ Om-de-Hoek/app/(tabs)/_layout.tsx
@@ -22,7 +22,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Magic Button Page!",
+          title: "Home",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/" asChild>
--- Om-de-Hoek/components/PressableButton.tsx
+++ Om-de-Hoek/components/PressableButton.tsx
@@ -1,18 +1,26 @@
 import {Color} from "@/types/StyleOptions";
 
 type Props = {
-    onPress: () => void;
+    onPress: () => Promise<void>;
     title: string;
     background? : Color;
     textColor?: Color;
 }
 
-import { Pressable, Text, StyleSheet } from "react-native";
+import {Pressable, Text, GestureResponderEvent} from "react-native";
 
 export const PressableButton = ({ onPress, title, background = Color.BLUE, textColor = Color.WHITE}: Props) => {
+
+    const onButtonPress = async (e: GestureResponderEvent) => {
+        const button = e.currentTarget;
+        button.setNativeProps({ style: { opacity: 0.6 } });
+        await onPress();
+        button.setNativeProps({ style: { opacity: 1 } });
+    }
+
     return(
         <Pressable
-            onPress={onPress}
+            onPress={onButtonPress}
             className={`px-4 py-2 rounded-lg mb-4`}
             style={{ backgroundColor: background }}
         >
