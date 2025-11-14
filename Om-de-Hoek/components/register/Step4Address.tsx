d23c69e68e13346d2ba2d1166dd0018c5f0a865d
WoutSeverens-UCLL <r0933353@ucll.be>
2025-11-14 16:38:55 +0100

    added step4 component

---
+++ Om-de-Hoek/components/register/Step4Address.tsx
@@ -0,0 +1,90 @@
+import {useState, useEffect} from "react";
+import {Text, View} from "react-native";
+import {WrittenInput} from "@/components/WrittenInput";
+import {PressableButton} from "@/components/PressableButton";
+import {Color} from "@/types/StyleOptions";
+
+export interface AddressData {
+    streetName: string;
+    houseNumber: string;
+    municipality: string;
+    postalCode: string;
+}
+
+type Props = {
+    onNext: () => void;
+    onChange: (data: AddressData) => void;
+}
+
+export const Step4Address = ({ onNext, onChange }: Props) => {
+    const [streetName, setStreetName] = useState<string>('');
+    const [houseNumber, setHouseNumber] = useState<string>('');
+    const [municipality, setMunicipality] = useState<string>('');
+    const [postalCode, setPostalCode] = useState<string>('');
+    const [isValid, setIsValid] = useState<boolean>(false);
+
+    useEffect(() => {
+        const areFieldsFilled =
+            streetName.length > 0 &&
+            municipality.length > 0 &&
+            postalCode.length > 0;
+
+        const PostalCodeRegex = /^[1-9][0-9]{3}$/;
+        const isPostalCodeValid = PostalCodeRegex.test(postalCode);
+
+        const valid = areFieldsFilled && isPostalCodeValid;
+        setIsValid(valid);
+
+        if (valid) {
+            onChange({ streetName, houseNumber, municipality, postalCode });
+        }
+
+        }, [streetName, houseNumber, municipality, postalCode])
+
+
+    return (
+        <View className="flex-1 p-6">
+            <Text className="text-[16px] text-black font-comfortaa-semibold text-center mb-2">
+                Wat is uw adres?
+            </Text>
+            <Text className="text-[14px] text-gray text-center font-comfortaa-medium mb-10">
+                Waar mogen we komen eten?
+            </Text>
+
+            <WrittenInput
+                placeholder="Straatnaam"
+                value={streetName}
+                onChangeText={setStreetName}
+                inputType="default"
+            />
+
+            <WrittenInput
+                placeholder="Huisnummer (optioneel)"
+                value={houseNumber}
+                onChangeText={setHouseNumber}
+                inputType="default"
+            />
+
+            <WrittenInput
+                placeholder="Gemeente"
+                value={municipality}
+                onChangeText={setMunicipality}
+                inputType="default"
+            />
+
+            <WrittenInput
+                placeholder="Postcode"
+                value={postalCode}
+                onChangeText={setPostalCode}
+                inputType="default"
+            />
+
+            <PressableButton
+                onPress={async () => onNext()}
+                disabled={!isValid}
+                title="Verdergaan"
+                background={isValid ? Color.BLUE : Color.GRAY}
+                />
+        </View>
+    )
+}
