adb6198e14cb8e991e3a4e07e25313e8916be72d
WoutSeverens-UCLL <r0933353@ucll.be>
2025-11-14 16:36:05 +0100

    added styling and all the 6 steps

--- Om-de-Hoek/app/(auth)/register.tsx
+++ Om-de-Hoek/app/(auth)/register.tsx
@@ -1,16 +1,28 @@
-import React, {useRef, useState} from "react";
+import React, {useRef, useState, useCallback} from "react";
 import {RegisterBody} from "@/types/auth";
 import PagerView from "react-native-pager-view";
-import {Text, View} from "react-native";
+import {Text, View, Image} from "react-native";
 import { Stack } from "expo-router";
 import {Step1Email} from "@/components/register/Step1Email";
+import {Step2Name} from "@/components/register/Step2Name";
+import {Step3BirthDate} from "@/components/register/Step3BirthDate";
+import {Step4Address} from "@/components/register/Step4Address";
+import {Step5PhoneNumber} from "@/components/register/Step5PhoneNumber";
+import {Step6Password} from "@/components/register/Step6Password";
+import {ProgressIndicator} from "@/components/ProgressIndicator";
+
+const totalSteps = 6
 
 export default function RegisterPage() {
     const [data, setData] = useState<RegisterBody>({
         email: '',
-        username: '',
+        firstName: '',
+        lastName: '',
+        streetName: '',
+        houseNumber: '',
+        municipality: '',
+        postalCode: '',
         password: '',
-        confirmPassword: '',
         birthDate: new Date(),
         phoneNumber: undefined
     })
@@ -32,23 +44,61 @@ export default function RegisterPage() {
             <Stack.Screen options={{title: "Maak een account"}} />
 
             <View className="items-center">
-                <Text className={"text-2xl font-bold"}>Om de Hoek</Text>
+                <Text className={"text-[24px] text-black font-comfortaa-semibold"}>Om de Hoek</Text>
+                <Text className={"text-[11px] text-gray font-comfortaa-medium"}>"Jouw buurt, jouw mensen, jouw plek."</Text>
+            </View>
+            <View className="items-center my-2">
+            <Image className="w-[256px] h-[256px]"
+            source={require('@/assets/images/logo.png')}/>
             </View>
 
             <PagerView
                 ref={pagerRef}
-                style={{ flex: 1, marginTop: 20 }}
+                style={{ flex: 1 }}
                 scrollEnabled={false}
                 onPageSelected={handlePageSelected}
             >
                 <View key="1">
                     <Step1Email
                         onNext={goToNextStep}
-                        onChange={(email) => setData({...data, email})}
+                        onChange={(email) => setData(prevData => ({...prevData, email}))}
                     />
                 </View>
 
+                <View key="2">
+                    <Step2Name
+                        onNext={goToNextStep}
+                        onChange={({firstName, lastName}) => setData(prevData => ({...prevData, firstName, lastName}))}
+                    />
+                </View>
 
+                <View key="3">
+                    <Step3BirthDate
+                        onNext={goToNextStep}
+                        onChange={(birthDate) => setData(prevData => ({...prevData, birthDate}))}
+                    />
+                </View>
+
+                <View key="4">
+                    <Step4Address
+                        onNext={goToNextStep}
+                        onChange={({ streetName, houseNumber, municipality, postalCode }) => setData(prevData => ({...prevData, streetName, houseNumber, municipality, postalCode }))}
+                    />
+                </View>
+
+                <View key="5">
+                    <Step5PhoneNumber
+                        onNext={goToNextStep}
+                        onChange={(phoneNumber) => setData(prevData => ({...prevData, phoneNumber}))}
+                    />
+                </View>
+
+                <View key="6">
+                    <Step6Password
+                        onNext={goToNextStep}
+                        onChange={(password) => setData(prevData => ({...prevData, password}))}
+                    />
+                </View>
             </PagerView>
         </View>
     )
