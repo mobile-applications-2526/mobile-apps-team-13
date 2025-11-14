848c4a6b7ca259be950c17c0873c5eec8982466c
WoutSeverens-UCLL <r0933353@ucll.be>
2025-11-14 16:37:02 +0100

    added and edited step1 page

--- Om-de-Hoek/components/register/Step1Email.tsx
+++ Om-de-Hoek/components/register/Step1Email.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {Text, View} from "react-native";
 import {WrittenInput} from "@/components/WrittenInput";
 import {PressableButton} from "@/components/PressableButton";
@@ -13,43 +13,39 @@ export const Step1Email = ({ onNext, onChange }: Props) => {
     const [email, setEmail] = useState<string>('');
     const [isValid, setIsValid] = useState<boolean>(false);
 
-    const handleEmailChange = (text: string) => {
-        const loweredText = text.toLowerCase();
-        setEmail(loweredText);
-        onChange(loweredText);
-    }
-
-    const validate = (): boolean => {
+    useEffect(() => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         const valid = emailRegex.test(email);
         setIsValid(valid);
-        return valid;
+
+        onChange(email)
+
+        }, [email])
+
+    const handleEmailChange = (text: string) => {
+        const loweredText = text.toLowerCase();
+        setEmail(loweredText);
     }
 
-    const handlePressNext = () => {
-        if(validate()){
-            onNext();
-        }
-    }
 
     return (
         <View className="flex-1 p-6">
-            <Text className="text-3xl font-bold text-center mb-2">
+            <Text className="text-[16px] text-black font-comfortaa-semibold text-center mb-2">
                 Maak een account
             </Text>
-            <Text className="text-lg text-gray-600 text-center mb-10">
-                En ontdek je buurt
+            <Text className="text-[14px] text-gray text-center font-comfortaa-medium mb-10">
+                En ontdek je buurt.
             </Text>
 
             <WrittenInput
-                placeholder="E-mailadres"
+                placeholder="E-mail"
                 value={email}
                 onChangeText={handleEmailChange}
                 inputType="email-address"
             />
 
             <PressableButton
-                onPress={async () => handlePressNext()}
+                onPress={async () => onNext()}
                 disabled={!isValid}
                 title="Verdergaan"
                 background={isValid ? Color.BLUE : Color.GRAY}
