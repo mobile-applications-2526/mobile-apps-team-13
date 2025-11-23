3ce9a3e4845d8263a8a0280a872d462ff9dde513
xanderdrvr <xander.deroover@student.ucll.be>
2025-11-23 18:32:18 +0100

    fix: enhance ValidationRow component styling

--- Om-de-Hoek/components/ValidationRow.tsx
+++ Om-de-Hoek/components/ValidationRow.tsx
@@ -1,10 +1,26 @@
 import { View, Text } from "react-native";
-import { CircleCheck } from "lucide-react-native"
+import { CircleCheck } from "lucide-react-native";
 
-export const ValidationRow = ({ isValid, text}: { isValid: boolean, text: string}) => {
-    return (
-    <View className="flex-row items-center mb-1">
-        {isValid ? <CircleCheck size={16} color="green" /> : <CircleCheck size={16} color="gray" />}
-        <Text className={`ml-2 ${isValid ? 'text-green' : 'text-gray'}`}>{text}</Text>
+export const ValidationRow = ({
+  isValid,
+  text,
+}: {
+  isValid: boolean;
+  text: string;
+}) => {
+  return (
+    <View className="flex-row items-center px-3 mb-2.5 rounded-lg bg-gray-50">
+      {isValid ? (
+        <CircleCheck size={18} color="green" strokeWidth={2.5} />
+      ) : (
+        <CircleCheck size={18} color="gray" strokeWidth={2.5} />
+      )}
+
+      <Text
+        className={`ml-3 font-comfortaa-regular text-sm ${isValid ? "text-green-600" : "text-gray"}`}
+      >
+        {text}
+      </Text>
     </View>
-)};
+  );
+};
