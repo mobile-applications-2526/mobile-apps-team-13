d3cd8e01a63d699deb64b4a2e48f3195f2770c8f
WoutSeverens-UCLL <r0933353@ucll.be>
2025-12-03 14:35:59 +0100

    LabeledInput component added to the Mijn gegevens page. Frontend only

--- Om-de-Hoek/app/(settings)/data.tsx
+++ Om-de-Hoek/app/(settings)/data.tsx
@@ -3,6 +3,8 @@ import SettingsHeader from "@/components/settings/SettingsHeader";
 import Back from "@/components/Back";
 import { ArrowLeft } from "lucide-react-native";
 import { useRouter } from "expo-router";
+import LabeledInput  from "@/components/settings/LabeledInput";
+import { useState } from "react";
 
 
 const PROFILE_PATH = "/(tabs)/profile";
@@ -10,6 +12,14 @@ const PROFILE_PATH = "/(tabs)/profile";
 
 export default function MyDataPage() {
     const router = useRouter();
+
+    const [email, setEmail] = useState("jan.peeters@example.com");
+    const [firstName, setFirstName] = useState("Jan");
+    const [lastName, setLastName] = useState("Peeters");
+    const [birthDate, setBirthDate] = useState("01-01-1990");
+    const [phoneNumber, setPhoneNumber] = useState("0123456789");
+    const [address1, setAddress1] = useState("Hoofdstraat 1");
+    const [address2, setAddress2] = useState("1234 AB, Stad");
 
 
     return (
@@ -20,6 +30,55 @@ export default function MyDataPage() {
                 <SettingsHeader
                     title="Mijn"
                     subtitle="Gegevens"
+                />
+                </View>
+                <View>
+                <LabeledInput
+                    label="Voornaam"
+                    value={firstName}
+                    onChange={setFirstName}
+                    editable={true}
+                />
+                <LabeledInput
+                    label="Achternaam"
+                    value={lastName}
+                    onChange={setLastName}
+                    editable={true}
+                />
+
+                <LabeledInput
+                    label="E-mailadres"
+                    value={email}
+                    onChange={setEmail}
+                    editable={true}
+                />
+
+                <LabeledInput
+                    label="Geboortedatum"
+                    value={birthDate}
+                    onChange={setBirthDate}
+                    editable={false}
+                />
+
+                <LabeledInput
+                    label="Telefoonnummer"
+                    value={phoneNumber}
+                    onChange={setPhoneNumber}
+                    editable={true}
+                />
+
+                <LabeledInput
+                    label="Adres 1"
+                    value={address1}
+                    onChange={setAddress1}
+                    editable={true}
+                />
+
+                <LabeledInput
+                    label="Adres 2"
+                    value={address2}
+                    onChange={setAddress2}
+                    editable={true}
                 />
                 </View>
             </View>
