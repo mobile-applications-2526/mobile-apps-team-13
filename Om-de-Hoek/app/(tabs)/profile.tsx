95a9064e52c27ec9410fe41371b610c4dd002e15
WoutSeverens-UCLL <r0933353@ucll.be>
2025-12-04 14:50:09 +0100

    Fetch the firstname and lastname out of the database

--- Om-de-Hoek/app/(tabs)/profile.tsx
+++ Om-de-Hoek/app/(tabs)/profile.tsx
@@ -1,23 +1,62 @@
 import {useAuth} from "@/components/auth/context/AuthContext";
-import {SafeAreaView, View, Text} from "react-native";
+import {SafeAreaView, View, Text, ActivityIndicator} from "react-native";
 import {PressableButton} from "@/components/PressableButton";
 import MenuItem from "@/components/settings/MenuItem";
 import SettingsHeader from "@/components/settings/SettingsHeader";
 import {Color} from "@/types/StyleOptions";
 import { UserRoundPen, Wrench, BellRing, UsersRound } from "lucide-react-native";
 import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import UserService from "@/services/UserService";
 
 export default function ProfilePage() {
 
+    const [firstName, setFirstName] = useState<string>("");
+    const [lastName, setLastName] = useState<string>("");
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
     const { signOut } = useAuth();
     const router = useRouter();
+    const { token } = useAuth();
+
+    useEffect(() => {
+        const fetchUserName = async () => {
+            try {
+                const response = await UserService.loggedInuser(token);
+                if (response.ok) {
+                    const data = await response.json();
+                    const fullName = data.userName || "";
+                    const nameParts = fullName.match(/[A-Z][a-z]*/g) || [fullName];
+                    setFirstName(nameParts[0] || "");
+                    setLastName(nameParts[1] || "");
+                } else {
+                    console.error("API Error:", response.status);
+                }
+            } catch (error) {
+                console.error("Failed to fetch user data", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchUserName();
+    }, [token]);
+
+    if (isLoading) {
+        return (
+            <View className="flex-1 justify-center items-center bg-white">
+                <ActivityIndicator size="large" color="#2548BC" />
+            </View>
+        );
+    }
+
 
     return(
         <View className="flex-1 bg-white px-6">
 
             <SettingsHeader
-                title="Jan"
-                subtitle="Peeters"
+                title={firstName}
+                subtitle={lastName}
             />
 
             <MenuItem
