48c6069c769c0f61bab1899d9ab15c85b83b61d2
WoutSeverens-UCLL <r0933353@ucll.be>
2025-12-04 14:48:45 +0100

    SelectInput component for choosing a language

---
+++ Om-de-Hoek/components/settings/SelectInput.tsx
@@ -0,0 +1,27 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import { ChevronDown } from "lucide-react-native";
+
+type Props = {
+    label: string;
+    value: string;
+    onPress: () => void;
+};
+export default function SelectInput({ label, value, onPress }: Props) {
+    return(
+        <View className="mb-6 ">
+            <Text className="text-black mb-2 font-comfortaa-bold">
+                {label}
+            </Text>
+
+            <TouchableOpacity
+                onPress={onPress}
+                className="flex-row items-center border justify-between border-black rounded-xl px-4 py-3"
+            >
+                <Text className="text-black font-comfortaa-regular">
+                    {value}
+                </Text>
+                <ChevronDown size={20} color="#100D08"/>
+            </TouchableOpacity>
+        </View>
+    )
+};
