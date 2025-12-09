7fa8a210c41d003f57204ea5661627697c77c3b7
WoutSeverens-UCLL <r0933353@ucll.be>
2025-12-09 10:08:59 +0100

    styling not complete

--- Om-de-Hoek/components/card/NeighborhoodGlassCard.tsx
+++ Om-de-Hoek/components/card/NeighborhoodGlassCard.tsx
@@ -5,13 +5,16 @@ import React from "react"
 
 type Props = {
   name: string
-  participants: number
+  participants?: number
+  action: "join" | "leave"
+  onJoin?: () => void
+  onLeave?: () => void
 }
 
-const NeighborhoodGlassCard: React.FC<Props> = ({ name, participants }) => {
+const NeighborhoodGlassCard: React.FC<Props> = ({ name, participants, action }) => {
   return (
     <View
-      className="mx-2 my-2 rounded-3xl bg-white p-2.5"
+      className="mx-2 my-2 rounded-lg bg-white py-4 px-4"
       style={{
         shadowColor: "#100D08",
         shadowOffset: { width: 0, height: 4 },
@@ -24,21 +27,21 @@ const NeighborhoodGlassCard: React.FC<Props> = ({ name, participants }) => {
         {/* Linkerkant */}
         <View className="flex-row items-center gap-2">
           <View className="w-10 h-10 rounded-lg bg-[#F4F4F4] items-center justify-center">
-            <UsersRound color="#100D08" size={24} />
+            <UsersRound color="#828282" size={24} />
           </View>
 
-          <Text className="text-black font-comfortaa-medium text-[12px]">{name}</Text>
+          <Text className="text-black font-comfortaa-bold text-[12px]">{name}</Text>
         </View>
 
         {/* Rechterkant */}
         <View className="flex-row items-center gap-2">
           <View className="flex-row items-center gap-1">
-            <Text className="text-black font-comfortaa-medium text-[12px]">{participants}</Text>
-            <UserRound color="#100D08" size={12} />
+            <UserRound color="#828282" size={12} />
+            <Text className="text-gray font-comfortaa-medium text-[12px]">{participants} leden</Text>
           </View>
 
           <TouchableOpacity className="bg-[#2548BC] px-2 py-1.5 rounded-xl">
-            <Text className="text-white font-comfortaa-bold text-[12px]">Deelnemen</Text>
+            <Text className="text-white font-comfortaa-bold text-[12px]">{action === "join" ? "Deelnemen" : "Verlaten"}</Text>
           </TouchableOpacity>
         </View>
       </View>
