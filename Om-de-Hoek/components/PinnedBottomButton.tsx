9afa4f50a37dd6b86c586be394d9e3d6e29e333d
WoutSeverens-UCLL <r0933353@ucll.be>
2025-12-19 15:10:11 +0100

    added pinned button component

---
+++ Om-de-Hoek/components/PinnedBottomButton.tsx
@@ -0,0 +1,18 @@
+import { TouchableOpacity, View, Text } from "react-native";
+import { Check } from "lucide-react-native";
+
+type Props = {
+    count?: number;
+    onNext?: () => void;
+};
+
+export default function PinnedBottomButton({ count, onNext }: Props) {
+    return (
+        <View className="absolute bottom-0 left-0 right-0 m-4">
+            <TouchableOpacity onPress={onNext} className="w-full flex-row justify-center elevation-md items-center bg-blue rounded-xl shadow-sm shadow-black/30 py-4">
+                <Check color={"#FFFF"} size={24} />
+                <Text className="text-white font-comfortaa-bold ml-3 text-lg">Klaar ({count} buurten geselecteerd) </Text>
+            </TouchableOpacity>
+        </View>
+    )
+} 
