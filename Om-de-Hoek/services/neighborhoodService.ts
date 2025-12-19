733bea8d2169dcdf75bd7b577bea5c91071b1727
WoutSeverens-UCLL <r0933353@ucll.be>
2025-12-19 15:11:42 +0100

    add and remove functions added

--- Om-de-Hoek/services/neighborhoodService.ts
+++ Om-de-Hoek/services/neighborhoodService.ts
@@ -1,7 +1,7 @@
 const API_URL = process.env.EXPO_PUBLIC_API_PATH;
 
-const fetchNeighborhoodsByPostalCode = async (postcode: string) => {
-    return await fetch(`${API_URL}/api/buurt/postcode/${postcode}`, {
+const fetchNeighborhoodsByPostalCode = async (postalcode: string) => {
+    return await fetch(`${API_URL}/api/neighborhood/postalcode/${postalcode}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -9,6 +9,28 @@ const fetchNeighborhoodsByPostalCode = async (postcode: string) => {
     });
 }
 
+const addToNeighborhood = async (id: string, token: string | null) => {
+    return await fetch(`${API_URL}/api/neighborhood/join/${id}`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    });
+};
+
+const removeFromNeighborhood = async (id: string, token: string | null) => {
+    return await fetch(`${API_URL}/api/neighborhood/leave/${id}`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    });
+}
+
 export default {
     fetchNeighborhoodsByPostalCode,
+    addToNeighborhood,
+    removeFromNeighborhood,
 }
