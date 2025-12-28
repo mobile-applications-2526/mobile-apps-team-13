0aa5f0ebdd642244f13d5c9289c89147c1ff9117
WoutSeverens-UCLL <r0933353@ucll.be>
2025-12-28 11:51:47 +0100

    added disabled and custom style

--- Om-de-Hoek/components/FloatingActionButton.tsx
+++ Om-de-Hoek/components/FloatingActionButton.tsx
@@ -4,18 +4,22 @@ type Props = {
   icon?: React.ReactNode;
   onPress?: () => void;
   isLoading?: boolean;
+  disabled?: boolean;
 };
 const FloatingActionButton: React.FC<Props> = ({
   icon,
   onPress,
   isLoading,
+  disabled,
 }) => {
   return (
     <TouchableOpacity
       onPress={onPress}
-      disabled={isLoading}
+      disabled={isLoading || disabled}
       activeOpacity={0.8}
-      className={`absolute bottom-8 right-6 h-14 w-14 items-center justify-center rounded-full bg-[#2548BC] shadow-lg shadow-black/30 ${isLoading ? "opacity-80" : ""}`}
+      className={`absolute bottom-8 right-6 h-14 w-14 items-center justify-center rounded-full shadow-lg shadow-black/30 
+        ${disabled ? "bg-gray" : "bg-[#2548BC]"} 
+        ${isLoading ? "opacity-80" : ""}`}
       style={{ elevation: 6, zIndex: 50 }}
     >
       {isLoading ? <ActivityIndicator color="white" /> : icon}
