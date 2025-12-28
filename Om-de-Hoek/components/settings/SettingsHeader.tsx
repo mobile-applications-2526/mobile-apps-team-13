819440354c80e2db3e1cc7bfcf3cae82ccc2e10a
WoutSeverens-UCLL <r0933353@ucll.be>
2025-12-28 19:54:42 +0100

    tabs removed

--- Om-de-Hoek/components/settings/SettingsHeader.tsx
+++ Om-de-Hoek/components/settings/SettingsHeader.tsx
@@ -1,16 +1,19 @@
 import { View, Text } from "react-native";
 
 type Props = {
-    title: string;
-    subtitle: string;
-}
+  title: string;
+  subtitle: string;
+};
 
-
-export default function SettingsHeader ({ title, subtitle }: Props) {
-    return (
-        <View className="mt-10 mb-8">
-            <Text className="font-comfortaa-bold text-black text-[24px]">{title}</Text>
-            <Text className="font-comfortaa-medium text-gray text-[14px] ml-8">{subtitle}</Text>
-        </View>
-    )
+export default function SettingsHeader({ title, subtitle }: Props) {
+  return (
+    <View className="mt-10 mb-8">
+      <Text className="font-comfortaa-bold text-black text-[24px]">
+        {title}
+      </Text>
+      <Text className="font-comfortaa-medium text-gray text-[14px] ml-8">
+        {subtitle}
+      </Text>
+    </View>
+  );
 }
