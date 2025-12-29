cd2aeda2edb15fe757534ae4a7cba74a2ae97d9e
Robbe Caerts <113980548+RobbeCaerts@users.noreply.github.com>
2025-12-29 16:43:42 +0100

    fix: fixed create new Adress logic

--- OmDeHoek-Backend/OmDeHoek/Model/Commands/Adressen/InsertAddress.cs
+++ OmDeHoek-Backend/OmDeHoek/Model/Commands/Adressen/InsertAddress.cs
@@ -5,5 +5,4 @@ public class InsertAddress
     public string Street { get; set; } = string.Empty;
     public string? HouseNumber { get; set; }
     public string PostalCode { get; set; } = string.Empty;
-    public string ResidentId { get; set; } = string.Empty;
 }
