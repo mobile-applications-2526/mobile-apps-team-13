b618038dd6d407f1b8f249887447ae37743f1e04
WoutSeverens-UCLL <r0933353@ucll.be>
2025-12-30 16:16:31 +0100

    removed top border

--- Om-de-Hoek/components/card/AdressCard.tsx
+++ Om-de-Hoek/components/card/AdressCard.tsx
@@ -137,7 +137,7 @@ const AdressCard = ({
             </View>
           </View>
 
-          <View className="flex-row justify-end items-center gap-4 mt-6 pt-2 border-t border-gray-100">
+          <View className="flex-row justify-end items-center gap-4 mt-6">
             {onCancel && (
               <Pressable onPress={onCancelEditing} className="px-4 py-2">
                 <Text className="font-comfortaa-semibold text-gray-500">
