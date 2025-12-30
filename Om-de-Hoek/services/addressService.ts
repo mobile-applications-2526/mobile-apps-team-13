2d45e08f2d0aa048c23997383eb7140991daeda3
WoutSeverens-UCLL <r0933353@ucll.be>
2025-12-30 16:18:08 +0100

    refactored tabs

--- Om-de-Hoek/services/addressService.ts
+++ Om-de-Hoek/services/addressService.ts
@@ -1,7 +1,14 @@
-import {Address, CreateAddressCommand, UpdateAddressCommand} from "@/types/address";
-import {fetchData} from "@/services/requestService";
+import {
+  Address,
+  CreateAddressCommand,
+  UpdateAddressCommand,
+} from "@/types/address";
+import { fetchData } from "@/services/requestService";
 
-const RegisterAddress = async (address: CreateAddressCommand, token: string) : Promise<Address> => {
+const RegisterAddress = async (
+  address: CreateAddressCommand,
+  token: string
+): Promise<Address> => {
   return await fetchData(`/address`, {
     method: "POST",
     headers: {
@@ -12,42 +19,48 @@ const RegisterAddress = async (address: CreateAddressCommand, token: string) : P
   });
 };
 
-const GetAllByAuthenticatedUser = async (token: string) : Promise<Address[]> => {
-    const response = await fetchData("/address/byloggedinuser", {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json',
-        }
-    })
+const GetAllByAuthenticatedUser = async (token: string): Promise<Address[]> => {
+  const response = await fetchData("/address/byloggedinuser", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
 
-    return response as Address[];
-}
+  return response as Address[];
+};
 
-const UpdateSingleAddress = async (address: UpdateAddressCommand, token: string) : Promise<Address> => {
-    return await fetchData("/address", {
-        method: 'PUT',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(address),
-    })
-}
+const UpdateSingleAddress = async (
+  address: UpdateAddressCommand,
+  token: string
+): Promise<Address> => {
+  return await fetchData("/address", {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(address),
+  });
+};
 
-const DeleteAddress = async (addressId: string, token: string) : Promise<void> => {
-    return await fetchData(`/address/${addressId}`, {
-        method: 'DELETE',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json',
-        }
-    })
-}
+const DeleteAddress = async (
+  addressId: string,
+  token: string
+): Promise<void> => {
+  return await fetchData(`/address/${addressId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+};
 
 export default {
-    RegisterAddress,
-    GetAllByAuthenticatedUser,
-    UpdateSingleAddress,
-    DeleteAddress
-}
+  RegisterAddress,
+  GetAllByAuthenticatedUser,
+  UpdateSingleAddress,
+  DeleteAddress,
+};
