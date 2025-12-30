cd78cb69caacc324a1289b6725aa69f1188ff244
WoutSeverens-UCLL <r0933353@ucll.be>
2025-12-30 16:17:48 +0100

    made a switch button component

---
+++ Om-de-Hoek/components/settings/SwitchButton.tsx
@@ -0,0 +1,25 @@
+import { Switch, Text, View } from "react-native";
+
+type Props = {
+  label?: string;
+  value?: boolean;
+  onValueChange?: (value: boolean) => void;
+};
+
+const SwitchButton = ({ label, value, onValueChange }: Props) => {
+  return (
+    <View className="flex-row items-center justify-between">
+      <Text className="text-black font-comfortaa-regular text-base">
+        {label}
+      </Text>
+      <Switch
+        value={value}
+        onValueChange={onValueChange}
+        thumbColor="#FFFFFF"
+        trackColor={{ false: "#BFBFBF", true: "#2548BC" }}
+      />
+    </View>
+  );
+};
+
+export default SwitchButton;
